b5c3f96a28b29e760ac1a71fce71c0ac91313a56
YamaguchiAtsushi <yamaguchi.atsushi2000@gmail.com>
2024-06-05 13:23:06 +0900

    add fullbody detection using caarscade

---
+++ script/fullbody_detect.py
@@ -0,0 +1,50 @@
+#https://chantastu.hatenablog.com/entry/2022/09/11/002035#5-%E5%85%A8%E8%BA%AB%E8%AA%8D%E8%AD%98
+
+#!/usr/bin/env python3
+
+import rospy
+import cv2
+import numpy as np
+from sensor_msgs.msg import Image
+from geometry_msgs.msg import Twist
+from cv_bridge import CvBridge, CvBridgeError
+import time
+
+
+
+# 体全体のカスケードファイル
+cascade = cv2.CascadeClassifier("../data_xml/haarcascade_fullbody.xml")
+#cascade = cv2.CascadeClassifier("/home/yamaguchi-a/catkin_ws/src/detect_pump/data_xml/haarcasca")
+
+
+
+def image_callback(msg):
+    bridge = CvBridge()
+    try:
+        #ROSのイメージメッセージをbgrのMat型に変換
+        cv_img = bridge.imgmsg_to_cv2(msg, "bgr8")
+        fullbodyrect = cascade.detectMultiScale(cv_img)
+        print('FullBody Coordinate1:', fullbodyrect[0])
+
+        if len(fullbodyrect) > 0:
+            for [x,y,w,d] in fullbodyrect:
+                cv2.rectangle(cv_img,(x,y),(x+w, y+d),(0,0,255),thickness=2)
+        #cv_img = bridge.imgmsg_to_cv2(msg)  #ROSのイメージメッセージをMat型に変換
+
+#######　処理を追加してみよう
+        
+        cv2.imshow("hogehoge", cv_img)
+        cv2.waitKey(1)
+        
+    except Exception as e:
+        print(e)
+
+
+
+def main():
+    rospy.init_node("camera_node")
+    rospy.Subscriber("/usb_cam/image_raw", Image, image_callback)
+    rospy.spin()
+
+if __name__ == "__main__":
+    main()
